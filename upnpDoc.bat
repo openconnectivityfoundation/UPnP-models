c9621bcb0fb5805e30ecde6ccda0a7b050ea68ac
Rick Bell <richard.s.bell@intel.com>
2017-01-30 17:17:35 -0800

    Fixed properties not being added

--- upnpDoc.bat
+++ upnpDoc.bat
@@ -4,9 +4,10 @@ rem ==================================================
 
 rem set Template="u:\Standards\Groups\OIC\UPnP\OCF_UPnP_Resource_v.0.1.docx"
 
-set UPnPTemplateDoc="OCF_UPnP_Resource_v.0.1.docx"
+set UPnPTemplateDoc="OCF_UPnP_Resource_v.0.3.docx"
+rem set UPnPTemplateDoc="OCF_SpecTemplate-Base.docx"
 set TemplateDir="C:\Users\rsbell\Downloads\OCF-Documents\"
-set UPnPModelsDir="H:\work\iotivity-master\oneiota\UPnP-models"
+set UPnPModelsDirDir="H:\work\iotivity-master\oneiota\UPnP-models"
 
 rem choice of application: python or exectuable
 set cmd=C:\Python27\python.exe  raml2doc-met.py
@@ -15,23 +16,27 @@ rem set cmd=raml2doc.exe
 rem make sure that correct template is used
 copy %TemplateDir%\%UPnPTemplateDoc%  .\%UPnPTemplateDoc%
 
+rem Copy all raml and json files
+copy %UPnPModelsDirDir%\*.raml  .
+copy %UPnPModelsDirDir%\*.json  .
+
 rem UPnP Resources 
-copy %UPnPModels%\upnpDevice.raml  .
+rem copy %UPnPModelsDir%\upnpDevice.raml  .
 %cmd% -raml upnpDevice.raml -resource /upnpDeviceResURI -docx %UPnPTemplateDoc%
 
-copy %UPnPModels%\upnpService.raml  .
+rem copy %UPnPModelsDir%\upnpService.raml  .
 %cmd% -raml upnpService.raml -resource /upnpServiceResURI -docx upnpDevice.raml.docx
 
-copy %UPnPModels%\upnpAction.raml  .
+rem copy %UPnPModelsDir%\upnpAction.raml  .
 %cmd% -raml upnpAction.raml -resource /upnpActionResURI -docx upnpService.raml.docx
 
-copy %UPnPModels%\upnpStateVariable.raml  .
+rem copy %UPnPModelsDir%\upnpStateVariable.raml  .
 %cmd% -raml upnpStateVariable.raml -resource /upnpStateVariableResURI -docx upnpAction.raml.docx
 
-copy %UPnPModels%\upnpGeneric.raml  .
+rem copy %UPnPModelsDir%\upnpGeneric.raml  .
 %cmd% -raml upnpGeneric.raml -resource /upnpGenericResURI -docx upnpStateVariable.raml.docx
 
-copy %UPnPModels%\icon.raml  .
+rem copy %UPnPModelsDir%\icon.raml  .
 %cmd% -raml icon.raml -resource /IconResURI -docx upnpGeneric.raml.docx
 
 copy  icon.raml.docx %TemplateDir%\%UPnPTemplateDoc%_generated.docx
